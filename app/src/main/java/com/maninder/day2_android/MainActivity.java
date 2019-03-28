1c9de842d42cc6d85748ed503cf23e633d2848ae
Maninder <maninder.tuli126@gmail.com>
2019-03-28 19:26:20 -0400

    Login Layout (Linear - Vertical and Linear - HorZ.) and code for Login

--- app/src/main/java/com/maninder/day2_android/MainActivity.java
+++ app/src/main/java/com/maninder/day2_android/MainActivity.java
@@ -2,12 +2,39 @@ package com.maninder.day2_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editEmail;
+    private EditText editPassword;
+    private Button buttonSign;
+    private Button buttonClear;
+    private TextView errorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editpassword);
+        errorMessage = findViewById(R.id.labelMessage);
+
+        buttonClear = findViewById(R.id.buttonClear);
+        buttonSign = findViewById(R.id.buttonSignIn);
+
+        buttonSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editEmail.getText().toString();
+                Toast.makeText(MainActivity.this,email, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
